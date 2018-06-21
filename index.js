24c4220e8eb095c9d2dd467e66006bcfdc2d8cef
Mihir Chaturvedi <plibither8@gmail.com>
2018-06-21 22:44:11 +0530

    index: Very, very rough working version complete

--- index.js
+++ index.js
@@ -1,26 +1,20 @@
-var stdin = process.stdin;
+const getchar = async () => {
 
-// without this, we would only get streams once enter is pressed
-stdin.setRawMode(true);
+    process.stdin.setRawMode(true);
+    process.stdin.setEncoding('utf8');
 
-// resume stdin in the parent process (node app won't quit all by itself
-// unless an error or process.exit() happens)
-stdin.resume();
+    return new Promise(resolve => process.stdin.once('data', data => {
 
-// i don't want binary, do you?
-stdin.setEncoding('utf8');
+        const byteArray = [...data];
+        if (byteArray.length > 0 && byteArray[0] === 3) {
+            console.log('^C');
+            process.exit(0);
+        }
+        process.stdin.setRawMode(false);
+        resolve(data);
 
-let i = 0;
+    }));
 
-// on any data into stdin
-stdin.on('data', function (key) {
-    // ctrl-c ( end of text )
-    if (key === '\u0003') {
-        process.exit();
-    }
-    // write the key to stdout all normal like
-    process.stdout.write(key+'\n');
-    if (++i === 10) {
-        stdin.pause();
-    }
-});
+};
+
+exports.getchar = getchar;
